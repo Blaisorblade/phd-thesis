15ce78317c43e152f9e099b71573bc2e9c5a9569
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2017-02-02 15:53:09 +0100

    Delete old stuff

--- pldi14/package_paper_sources_test.sh
+++
@@ -1,9 +0,0 @@
-cd $(dirname "$0")
-
-(cd ..; [ -d arxivPaper ] && { rm -rf arxivPaper-sav; cp -a arxivPaper arxivPaper-sav; })
-
-./package_paper_sources.sh
-
-cd ..
-
-[ -d arxivPaper-sav ] && { diff -ur --brief arxivPaper arxivPaper-sav && echo "Output did not change; diff exitcode $?" || echo "Output did change!"; }
