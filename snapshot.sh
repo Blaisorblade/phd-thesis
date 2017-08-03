7824dd6761b431d875aa9bc0b12a3f4053ea2cdd
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2017-08-03 18:58:53 +0200

    Don't save snapshots on Dropbox

--- snapshot.sh
+++ snapshot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-new_snapshot=~/Dropbox/Drafts/thesis-main-$(timestamp)-g$(git describe --always).pdf
+#new_snapshot=~/Dropbox/Drafts/thesis-main-$(timestamp)-g$(git describe --always).pdf
+new_snapshot=thesis-main-${1/%/-}$(timestamp)-g$(git describe --always).pdf
 cp thesis-main.pdf ${new_snapshot}
 skim-revert ${new_snapshot}
